4268d269693e9ed64ca30c921a34ea9a3887719c
studiofraternali <studio.fraternali@gmail.com>
2026-03-05 15:54:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 window.addEventListener("load", function(){
 
+    const stack = document.querySelector(".stack");
+    const sections = document.querySelectorAll(".section");
     const authors = document.querySelectorAll(".author");
 
-    authors.forEach((author, index) => {
+    let index = 0;
+
+    function rotateSections(){
+
+        index++;
+
+        if(index >= sections.length){
+            index = 0;
+        }
+
+        stack.style.transform = "translateY(-" + (index * 100) + "vh)";
+    }
+
+    setInterval(rotateSections, 5000);
+
+
+    // AUTORE ANIMATO
+
+    authors.forEach((author, i) => {
 
         setTimeout(()=>{
             author.classList.add("visible");
-        }, 900 + (index * 200));
+        }, 900 + (i * 200));
 
     });
 
 
-    // ===== MENU MOBILE =====
+    // MENU MOBILE
 
     const hamburger = document.getElementById("hamburger");
     const mobileMenu = document.getElementById("mobileMenu");
