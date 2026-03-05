663be5dc497227dc14272d504e0865193cd596c8
studiofraternali <studio.fraternali@gmail.com>
2026-03-05 15:34:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
-// ===== AUTORE DOPO LA CITAZIONE =====
+document.addEventListener("DOMContentLoaded", function(){
 
-const authors = document.querySelectorAll(".author");
+    const authors = document.querySelectorAll(".author");
 
-authors.forEach((author, i) => {
+    authors.forEach((author) => {
 
-    setTimeout(()=>{
-        author.classList.add("visible");
-    }, 1200 + (i * 300));
+        const observer = new IntersectionObserver(entries => {
+
+            entries.forEach(entry => {
+
+                if(entry.isIntersecting){
+
+                    setTimeout(()=>{
+                        author.classList.add("visible");
+                    }, 800);
+
+                }
+
+            });
+
+        },{
+            threshold:0.5
+        });
+
+        observer.observe(author);
+
+    });
+
+
+    // ===== MENU MOBILE =====
+
+    const hamburger = document.getElementById("hamburger");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    if (hamburger && mobileMenu) {
+
+        hamburger.addEventListener("click", function (e) {
+            e.stopPropagation();
+            hamburger.classList.toggle("active");
+            mobileMenu.classList.toggle("active");
+        });
+
+        document.addEventListener("click", function (e) {
+            if(!mobileMenu.contains(e.target) && !hamburger.contains(e.target)){
+                mobileMenu.classList.remove("active");
+                hamburger.classList.remove("active");
+            }
+        });
+
+    }
 
 });
-
-
-// ===== MENU MOBILE =====
-
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-
-if (hamburger && mobileMenu) {
-
-    hamburger.addEventListener("click", function (e) {
-        e.stopPropagation();
-        hamburger.classList.toggle("active");
-        mobileMenu.classList.toggle("active");
-    });
-
-    document.addEventListener("click", function (e) {
-        if(!mobileMenu.contains(e.target) && !hamburger.contains(e.target)){
-            mobileMenu.classList.remove("active");
-            hamburger.classList.remove("active");
-        }
-    });
-
-}
