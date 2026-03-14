a548db98c2113e9253cf0649f875aad8a1e22307
studiofraternali <studio.fraternali@gmail.com>
2026-03-14 09:31:21 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", function(){
 
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-const mobileClose = document.getElementById("mobileClose");
-
-if(!hamburger || !mobileMenu) return;
-
-hamburger.addEventListener("click", function(){
-mobileMenu.classList.add("active");
-});
-
-if(mobileClose){
-mobileClose.addEventListener("click", function(){
-mobileMenu.classList.remove("active");
-});
-}
-
-});
